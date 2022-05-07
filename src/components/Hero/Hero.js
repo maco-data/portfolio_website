ed6f61a0f2fc7f2f01ce3bdc968b2725feb68bd5
maco-data <mcospino@gmail.com>
2022-05-07 22:30:26 +0200

    Complete first iteration of Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from "react"
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+	Section,
+	SectionText,
+	SectionTitle,
+} from "../../styles/GlobalComponents"
+import Button from "../../styles/GlobalComponents/Button"
+import { LeftSection } from "./HeroStyles"
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = () => (
+	<Section row nopadding>
+		<LeftSection>
+			<SectionTitle main center>
+				Hi, I'm Marco <br />
+				Welcome to my Portfolio
+			</SectionTitle>
+			<SectionText>
+				“Maybe stories are just data with a soul.” <br />
+				&nbsp;&nbsp;&nbsp;&nbsp;— Brené Brown
+			</SectionText>
+			<Button
+				onClick={() =>
+					(window.location =
+						"https://www.linkedin.com/in/maco-network/")
+				}>
+				Let's get in contact
+			</Button>
+		</LeftSection>
+	</Section>
+)
 
-export default Hero;
+export default Hero
