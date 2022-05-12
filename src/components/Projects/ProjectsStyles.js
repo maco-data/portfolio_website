09ce052040f77719f2bbc8dcfc4309edcb63ea4d
maco-data <mcospino@gmail.com>
2022-05-12 23:55:31 +0200

    Fix styling

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -18,7 +18,7 @@ export const GridContainer = styled.section`
 		display: flex;
 		flex-direction: column;
 		padding: 2rem;
-		padding-bottom: 0;
+		padding-bottom: 2rem;
 	}
 `
 export const BlogCard = styled.div`
@@ -70,7 +70,7 @@ export const CardInfo = styled.p`
 	line-height: 24px;
 	text-align: justify;
 	@media ${props => props.theme.breakpoints.sm} {
-		padding: 0.3rem;
+		padding: 0 3.125rem;
 	}
 `
 
