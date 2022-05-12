82eacc6bed3f94b494b933b095552cb24d345dda
maco-data <mcospino@gmail.com>
2022-05-12 23:29:59 +0200

    Complete footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react"
+import {
+	AiFillGithub,
+	AiFillDribbbleCircle,
+	AiFillLinkedin,
+} from "react-icons/ai"
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles"
+import {
+	CompanyContainer,
+	FooterWrapper,
+	LinkColumn,
+	LinkItem,
+	LinkList,
+	LinkTitle,
+	Slogan,
+	SocialContainer,
+	SocialIconsContainer,
+} from "./FooterStyles"
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
-};
+	return (
+		<FooterWrapper>
+			<LinkList>
+				<LinkColumn>
+					<LinkTitle>Email</LinkTitle>
+					<LinkItem href="mailto:mcospino@gmail.com">
+						mcospino@gmail.com
+					</LinkItem>
+				</LinkColumn>
+			</LinkList>
+			<SocialIconsContainer>
+				<CompanyContainer>
+					<Slogan>
+						“The goal is to turn data into information, and
+						information into insight.” — Carly Fiorina
+					</Slogan>
+				</CompanyContainer>
+				<SocialContainer>
+					<SocialIcons href="https://github.com/maco-data">
+						<AiFillGithub size="3rem" />
+					</SocialIcons>
+					<SocialIcons href="https://www.linkedin.com/in/maco-network/">
+						<AiFillLinkedin size="3rem" />
+					</SocialIcons>
+					<SocialIcons href="https://dribbble.com/maco-design">
+						<AiFillDribbbleCircle size="3rem" />
+					</SocialIcons>
+				</SocialContainer>
+			</SocialIconsContainer>
+		</FooterWrapper>
+	)
+}
 
-export default Footer;
+export default Footer
