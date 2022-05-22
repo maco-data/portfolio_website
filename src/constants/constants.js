6f07ce470b304d0c850a451b9afa597bbe09054b
maco-data <mcospino@gmail.com>
2022-05-22 21:47:17 +0200

    Commnet out  4th project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -22,23 +22,23 @@ export const projects = [
 	{
 		title: "Web Scraping Data and Machine Learning",
 		description:
-			"This project was a Machine Learning practice. I scrape the data then use it to learned how to predict the winner of football matches in the English Premier League (EPL). Who does not love football? and what better to know if your favorite team is more likely to win against your friends favorite team.",
+			"This project was a Machine Learning practice. I scraped the data and then used it to learn how to predict the winner of football matches in the English Premier League (EPL). Who does not love football? and what better to know if your favorite team is more likely to win than your friends’ favorite team. I used only the last 5  years (2017 to 2022).",
 		image: "/images/3.png",
 		tags: ["Python", "Jupyter Notebook", "Scikit Learn", "Beautiful Soup"],
 		source: "https://github.com/maco-data/ML_EPL",
 		visit: "https://google.com",
 		id: 2,
 	},
-	{
-		title: "Unichat",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam tortor sit amet ipsum fringilla vestibulum. Vivamus pellentesque finibus massa, sed tincidunt dui malesuada nec. Duis consectetur blandit eros sit amet porta. Aliquam sed nibh mollis, interdum velit ut, porttitor felis. Duis scelerisque dapibus purus a pharetra. Curabitur vehicula massa.",
-		image: "/images/4.jpg",
-		tags: ["React", "ChatEngine", "Firebase"],
-		source: "https://google.com",
-		visit: "https://google.com",
-		id: 3,
-	},
+	// {
+	// 	title: "Unichat",
+	// 	description:
+	// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam tortor sit amet ipsum fringilla vestibulum. Vivamus pellentesque finibus massa, sed tincidunt dui malesuada nec. Duis consectetur blandit eros sit amet porta. Aliquam sed nibh mollis, interdum velit ut, porttitor felis. Duis scelerisque dapibus purus a pharetra. Curabitur vehicula massa.",
+	// 	image: "/images/4.jpg",
+	// 	tags: ["React", "ChatEngine", "Firebase"],
+	// 	source: "https://google.com",
+	// 	visit: "https://google.com",
+	// 	id: 3,
+	// },
 ]
 
 export const TimeLineData = [
