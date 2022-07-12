33a6287eb5cff14da76b14442d872d805239ffc1
maco-data <mcospino@gmail.com>
2022-07-12 22:32:57 +0200

    Change deployment path

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -1,158 +1,140 @@
-(function() {
+"use strict";
+(() => {
 var exports = {};
 exports.id = 660;
 exports.ids = [660];
 exports.modules = {
 
-/***/ 4034:
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ 4178:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": function() { return /* binding */ MyDocument; }
+/* harmony export */   "default": () => (/* binding */ MyDocument)
 /* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var next_document__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6859);
-/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9914);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(7518);
 /* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_2__);
 
 
 
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-
-
-class MyDocument extends next_document__WEBPACK_IMPORTED_MODULE_1__.default {
-  static async getInitialProps(ctx) {
-    const sheet = new styled_components__WEBPACK_IMPORTED_MODULE_2__.ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles( /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(App, _objectSpread({}, props)))
-      });
-
-      const initialProps = await next_document__WEBPACK_IMPORTED_MODULE_1__.default.getInitialProps(ctx);
-      return _objectSpread(_objectSpread({}, initialProps), {}, {
-        styles: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
-          children: [initialProps.styles, sheet.getStyleElement()]
-        })
-      });
-    } finally {
-      sheet.seal();
+class MyDocument extends next_document__WEBPACK_IMPORTED_MODULE_1__["default"] {
+    static async getInitialProps(ctx) {
+        const sheet = new styled_components__WEBPACK_IMPORTED_MODULE_2__.ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+        try {
+            ctx.renderPage = ()=>originalRenderPage({
+                    enhanceApp: (App)=>(props)=>sheet.collectStyles(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(App, {
+                                ...props
+                            }))
+                });
+            const initialProps = await next_document__WEBPACK_IMPORTED_MODULE_1__["default"].getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+                    children: [
+                        initialProps.styles,
+                        sheet.getStyleElement()
+                    ]
+                })
+            };
+        } finally{
+            sheet.seal();
+        }
     }
-  }
+    render() {
+        return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(next_document__WEBPACK_IMPORTED_MODULE_1__.Html, {
+            lang: "en-GB",
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.Head, {
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+                        href: "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap",
+                        rel: "stylesheet"
+                    })
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("body", {
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.Main, {}),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.NextScript, {})
+                    ]
+                })
+            ]
+        });
+    }
+};
 
-  render() {
-    return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(next_document__WEBPACK_IMPORTED_MODULE_1__.Html, {
-      lang: "en-GB",
-      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.Head, {
-        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
-          href: "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap",
-          rel: "stylesheet"
-        })
-      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("body", {
-        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.Main, {}), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.NextScript, {})]
-      })]
-    });
-  }
-
-}
-
-/***/ }),
-
-/***/ 227:
-/***/ (function(module) {
-
-"use strict";
-module.exports = require("next/dist/next-server/lib/constants.js");;
 
 /***/ }),
 
-/***/ 3932:
-/***/ (function(module) {
+/***/ 4140:
+/***/ ((module) => {
 
-"use strict";
-module.exports = require("next/dist/next-server/lib/document-context.js");;
-
-/***/ }),
-
-/***/ 816:
-/***/ (function(module) {
-
-"use strict";
-module.exports = require("next/dist/next-server/lib/head-manager-context.js");;
+module.exports = require("next/dist/server/get-page-files.js");
 
 /***/ }),
 
-/***/ 7579:
-/***/ (function(module) {
+/***/ 9716:
+/***/ ((module) => {
 
-"use strict";
-module.exports = require("next/dist/next-server/lib/utils.js");;
-
-/***/ }),
-
-/***/ 6171:
-/***/ (function(module) {
-
-"use strict";
-module.exports = require("next/dist/next-server/server/get-page-files.js");;
+module.exports = require("next/dist/server/htmlescape.js");
 
 /***/ }),
 
-/***/ 5105:
-/***/ (function(module) {
+/***/ 6368:
+/***/ ((module) => {
 
-"use strict";
-module.exports = require("next/dist/next-server/server/utils.js");;
-
-/***/ }),
-
-/***/ 4229:
-/***/ (function(module) {
-
-"use strict";
-module.exports = require("prop-types");;
+module.exports = require("next/dist/server/utils.js");
 
 /***/ }),
 
-/***/ 9297:
-/***/ (function(module) {
+/***/ 6724:
+/***/ ((module) => {
 
-"use strict";
-module.exports = require("react");;
-
-/***/ }),
-
-/***/ 5282:
-/***/ (function(module) {
-
-"use strict";
-module.exports = require("react/jsx-runtime");;
+module.exports = require("next/dist/shared/lib/constants.js");
 
 /***/ }),
 
-/***/ 9914:
-/***/ (function(module) {
+/***/ 2796:
+/***/ ((module) => {
 
-"use strict";
-module.exports = require("styled-components");;
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
 
 /***/ }),
 
-/***/ 1168:
-/***/ (function(module) {
+/***/ 8743:
+/***/ ((module) => {
 
-"use strict";
-module.exports = require("styled-jsx/server");;
+module.exports = require("next/dist/shared/lib/html-context.js");
+
+/***/ }),
+
+/***/ 8524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 7518:
+/***/ ((module) => {
+
+module.exports = require("styled-components");
 
 /***/ })
 
@@ -162,8 +144,8 @@ module.exports = require("styled-jsx/server");;
 // load runtime
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
-var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-var __webpack_exports__ = __webpack_require__.X(0, [859], function() { return __webpack_exec__(4034); });
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [952,859], () => (__webpack_exec__(4178)));
 module.exports = __webpack_exports__;
 
 })();
--- .next/server/pages/api/hello.js
+++ .next/server/pages/api/hello.js
@@ -1,20 +1,24 @@
-(function() {
+"use strict";
+(() => {
 var exports = {};
 exports.id = 453;
 exports.ids = [453];
 exports.modules = {
 
-/***/ 4129:
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ 4665:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-/* harmony default export */ __webpack_exports__["default"] = ((req, res) => {
-  res.status(200).json({
-    name: 'John Doe'
-  });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((req, res)=>{
+    res.status(200).json({
+        name: "John Doe"
+    });
 });
+
 
 /***/ })
 
@@ -22,10 +26,10 @@ __webpack_require__.r(__webpack_exports__);
 ;
 
 // load runtime
-var __webpack_require__ = require("../../webpack-runtime.js");
+var __webpack_require__ = require("../../webpack-api-runtime.js");
 __webpack_require__.C(exports);
-var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-var __webpack_exports__ = (__webpack_exec__(4129));
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(4665));
 module.exports = __webpack_exports__;
 
 })();
