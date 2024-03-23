aa7b95a5003177b66896be838486d9a8fd916999
maco-data <mcospino@gmail.com>
2024-03-23 00:16:25 -0500

    Tried fix issues with links in some js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react"
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+import { Container } from "./LayoutStyles"
 
-export const Layout = ({children}) => {
-  return (
-    <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
-    </Container>
-  )
+export const Layout = ({ children }) => {
+	return (
+		<Container>
+			<Header />
+			<title>Marco Camarena</title>
+			<main>{children}</main>
+			<Footer />
+		</Container>
+	)
 }
